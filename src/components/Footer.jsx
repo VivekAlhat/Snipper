010b1c94fa837f8dfaaaccb6ff274abab939c0a7
Vivek Alhat <vivekalhat14@outlook.com>
2022-03-06 16:52:15 +0530

    :hammer: Adjust Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import Logo from "../Assets/Logo.svg";
 
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-between p-5 sm:flex-col sm:items-start sm:px-48 md:px-10 2xl:px-96 sm:gap-3 bg-white">
+    <footer className="flex flex-col items-start justify-between gap-3 p-5 sm:flex-row sm:items-start sm:px-48 md:px-10 2xl:px-96 sm:gap-5 bg-white">
       <img src={Logo} alt="Snipper Logo" />
-      <div className="flex items-center ml-5 gap-3 sm:gap-5 sm:ml-0">
+      <div className="flex items-center gap-3">
         <p className="text-gray-900 w-fit cursor-pointer">
           Terms and Conditions
         </p>
