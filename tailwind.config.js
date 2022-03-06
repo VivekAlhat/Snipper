018a77cdf5ceae46fc1029c65c8f4b694cbd1daa
Vivek Alhat <vivekalhat14@outlook.com>
2022-03-06 16:34:48 +0530

    Create Responsive Design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       animation: {
@@ -13,6 +13,9 @@ module.exports = {
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
+      },
+      backgroundImage: {
+        "hero-bg": "url('./src/Assets/Background.png')",
       },
     },
   },
